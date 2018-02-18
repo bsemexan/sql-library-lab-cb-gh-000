188b6334a78e87c4247ba7e155b610843a25acc6
Berry  Semexan <bsemexan@gmail.com>
2018-02-18 05:16:04 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -16,7 +16,7 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "SELECT species, COUNT(*) FROM characters;"
+  "SELECT MAX(species), COUNT(*) FROM characters;"
 end
 
 def select_name_and_series_subgenres_of_authors
