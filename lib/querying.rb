e0e093e1ad80269d5160e8a190988ec52c36c2a7
Berry  Semexan <bsemexan@gmail.com>
2018-02-18 05:36:42 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -27,7 +27,9 @@ def select_name_and_series_subgenres_of_authors
   "SELECT authors.name, subgenres.name
   FROM authors
   JOIN series
-  ON authors.id = series.author_id;"
+  ON authors.id = series.author_id
+  JOIN subgenres
+  ON subgenres.id = series.subgenre_id;"
 end
 
 def select_series_title_with_most_human_characters
