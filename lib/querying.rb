9373b24dbda327508f060c40c4e85ead3a590b63
Berry  Semexan <bsemexan@gmail.com>
2018-02-18 05:06:38 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -8,9 +8,9 @@ def select_books_titles_and_years_in_first_series_order_by_year
 end
 
 def select_name_and_motto_of_char_with_longest_motto
-  "SELECT name, motto
+  "SELECT TOP name, motto
   FROM characters
-  WHERE LEGNTH(motto) = (SELECT MAX(LENGTH(motto) FROM characters);"
+  ORDER BY LENGTH(motto) DESC;
 end
 
 
