fc290cb32b28322759c71cbcef4c6b86d16412ea
Berry  Semexan <bsemexan@gmail.com>
2018-02-18 05:44:36 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -39,7 +39,7 @@ def select_series_title_with_most_human_characters
   ON series.id = characters.series_id
   GROUP BY species
   ORDER BY species DESC
-  LIMIT 1;"
+  ;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
